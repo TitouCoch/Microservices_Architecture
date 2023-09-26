9b41097ccf6203a2b0fe09f5f76971b033c48d6e
EliasMorio <elias14160@gmail.com>
2023-09-26 17:04:33 +0200

    feat(times): add times endpoints

--- showtime/showtime.py
+++ showtime/showtime.py
@@ -8,12 +8,27 @@ PORT = 3202
 HOST = '0.0.0.0'
 
 with open('{}/databases/times.json'.format("."), "r") as jsf:
-   schedule = json.load(jsf)["schedule"]
+    schedule = json.load(jsf)["schedule"]
+
 
 @app.route("/", methods=['GET'])
 def home():
-   return "<h1 style='color:blue'>Welcome to the Showtime service!</h1>"
+    return "<h1 style='color:blue'>Welcome to the Showtime service!</h1>"
+
+
+@app.route("/showtimes", methods=["GET"])
+def get_times():
+    return make_response(jsonify(schedule), 200)
+
+
+@app.route("/showmovies/<date>", methods=["GET"])
+def get_movies_by_date(date):
+    for d in schedule:
+        if str(d['date']) == str(date):
+            return make_response(jsonify(d), 200)
+    return make_response({"error": "No date found"})
+
 
 if __name__ == "__main__":
-   print("Server running in port %s"%(PORT))
-   app.run(host=HOST, port=PORT)
+    print("Server running in port %s" % (PORT))
+    app.run(host=HOST, port=PORT)
