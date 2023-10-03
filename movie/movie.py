8c842992859e653b20498e6aec2cea6deeb6accf
EliasMorio <elias14160@gmail.com>
2023-10-03 14:59:31 +0200

    add basic graphql config

--- movie/movie.py
+++ movie/movie.py
@@ -1,6 +1,6 @@
 from ariadne import graphql_sync, make_executable_schema, load_schema_from_path, ObjectType, QueryType, MutationType
 from ariadne.constants import PLAYGROUND_HTML
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, make_response
 
 import resolvers as r
 
@@ -8,8 +8,11 @@ PORT = 3001
 HOST = '0.0.0.0'
 app = Flask(__name__)
 
-# todo create elements for Ariadne
-
+type_defs = load_schema_from_path("movie.graphql")
+query = QueryType()
+movie = ObjectType('Movie')
+query.set_field("movie_with_id", r.movie_with_id)
+schema = make_executable_schema(type_defs, query, movie)
 
 # root message
 @app.route("/", methods=['GET'])
@@ -25,7 +28,16 @@ def playground():
     
 @app.route('/graphql', methods=['POST'])
 def graphql_server():
-    # todo to complete
+    data = request.get_json()
+    success, result = graphql_sync(
+        schema,
+        data,
+        context_value=None,
+        debug=app.debug
+    )
+    status_code = 200 if success else 400
+    return jsonify(result), status_code
+
 
 if __name__ == "__main__":
     print("Server running in port %s"%(PORT))
---
+++ movie/resolvers.py
@@ -0,0 +1,9 @@
+import json
+
+
+def movie_with_id(_, info, _id):
+    with open('{}/data/movies.json'.format("."), "r") as file:
+        movies = json.load(file)
+        for movie in movies['movies']:
+            if movie['id'] == _id:
+                return movie
