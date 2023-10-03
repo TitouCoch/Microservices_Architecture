6fa21ec60420ab966f661485e3f57b726080e9d7
EliasMorio <elias14160@gmail.com>
2023-10-03 21:23:07 +0200

    add graphql request in user services

--- user/user.py
+++ user/user.py
@@ -1,92 +1,58 @@
-# REST API
 from flask import Flask, render_template, request, jsonify, make_response
 import requests
 import json
 from werkzeug.exceptions import NotFound
 
-# CALLING gRPC requests
-import grpc
-from concurrent import futures
-import booking_pb2
-import booking_pb2_grpc
-import movie_pb2
-import movie_pb2_grpc
-
-# CALLING GraphQL requests
-
 app = Flask(__name__)
 
-PORT = 3004
+PORT = 3203
 HOST = '0.0.0.0'
+BOOKING_SERVICE_BASE_URL = "http://172.16.137.68:3201"
+MOVIE_GRAPHQL_SERVICE_BASE_URL = "http://172.16.137.68:3001/graphql"
 
-with open('{}/databases/users.json'.format("."), "r") as jsf:
-   users = json.load(jsf)["users"]
+with open('{}/data/users.json'.format("."), "r") as jsf:
+    users = json.load(jsf)["users"]
 
 
 @app.route("/", methods=['GET'])
 def home():
-   return "<h1 style='color:blue'>Welcome to the User service!</h1>"
+    return "<h1 style='color:blue'>Welcome to the User service!</h1>"
 
 
-@app.route("/users", methods=['GET'])
-def get_json():
-   res = make_response(jsonify(users), 200)
-   return res
+def query_movie_with_id(id):
+    return f'''
+    query Movie_with_id {{
+        movie_with_id(_id: "{id}") {{
+        id
+        title
+        director
+        rating
+        actors {{
+            id
+            firstname
+            lastname
+            birthyear
+        }}
+        }}
+    }}'''
 
-
-@app.route("/users/<userid>", methods=['GET'])
-def get_info_movies_user(userid):
-   # Obtenez les données de réservation
-   booking_response = requests.get(f"{booking_service_url}/bookings/{userid}")
-   if booking_response.status_code != 200:
-      return make_response(jsonify({"error": "Booking data not found"}), 404)
-
-   booking = booking_response.json()
-
-   # Obtenez les données des films
-   movies_response = requests.get(f"{movies_service_url}/json")
-   if movies_response.status_code != 200:
-      return make_response(jsonify({"error": "Movie data not found"}), 404)
-
-   movies = movies_response.json()
-
-   # Créez une liste de films correspondants
-   result = []
-
-   for data in booking["dates"]:
-      for booking_movie in data["movies"]:
-         for movie in movies:
-            if movie["id"] == booking_movie:
-               result.append(movie)
-
-   res = make_response(jsonify(result), 200)
-   return res
-
-
-@app.route("/users/<userid>/<date>", methods=['GET'])
-def get_movies_for_user_and_date(userid, date):
-   booking_response = requests.get(f"{booking_service_url}/bookings/{userid}")
-
-   # Vérifiez si la réponse du service de réservation a retourné avec succès
-   if booking_response.status_code != 200:
-      return make_response(jsonify({"error": "Booking data not found"}), 404)
-
-   booking = booking_response.json()
-
-   result = []
-
-   for data in booking["dates"]:
-      if data["date"] == date:
-         for booking_movie in data["movies"]:
-            result.append(booking_movie)
-
-   if not result:
-      return make_response(jsonify({"error": "No reservation for this day"}), 404)
-
-   res = make_response(jsonify(result), 200)
-   return res
+@app.route("/bookingsdetails/<userid>", methods=["GET"])
+def get_bookings_details(userid):
+    r_bookings = requests.get(BOOKING_SERVICE_BASE_URL + '/bookings/' + userid)
+    print(r_bookings.json())
+    response = []
+    # movie_cache = {}
+    if r_bookings.status_code == 200:
+        for booking in r_bookings.json()['dates']:
+            movies_details = []
+            for movieid in booking['movies']:
+                r_movies = requests.post(MOVIE_GRAPHQL_SERVICE_BASE_URL,
+                                         json={'query': query_movie_with_id(movieid)})
+                movies_details.append(r_movies.json()['data']['movie_with_id'])
+            response.append({'date': booking['date'], 'movies': movies_details})
+    return make_response(jsonify(response), 200)
 
 
 if __name__ == "__main__":
-   print("Server running in port %s" % (PORT))
-   app.run(host=HOST, port=PORT)
+    print("Server running in port %s" % (PORT))
+    app.run(host=HOST, port=PORT)
