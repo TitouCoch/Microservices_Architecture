7c6d604bd91c744a36cee5cccac4d97de4d0e6b9
EliasMorio <elias14160@gmail.com>
2023-10-10 15:45:25 +0200

    add tuto

--- client/client.py
+++ client/client.py
@@ -4,9 +4,16 @@ import movie_pb2
 import movie_pb2_grpc
 
 
-def get_movie_by_id(stub,id):
+def get_movie_by_id(stub, id):
     movie = stub.GetMovieByID(id)
     print(movie)
+
+
+def get_list_movies(stub):
+    allmovies = stub.GetListMovies(movie_pb2.Empty())
+    for movie in allmovies:
+        print("Movie called %s" % (movie.title))
+
 
 def run():
     # NOTE(gRPC Python Team): .close() is possible on a channel and should be
@@ -19,7 +26,11 @@ def run():
         movieid = movie_pb2.MovieID(id="a8034f44-aee4-44cf-b32c-74cf452aaaae")
         get_movie_by_id(stub, movieid)
 
+        print("-------------- GetListMovies --------------")
+        get_list_movies(stub)
+
     channel.close()
+
 
 if __name__ == '__main__':
     run()
--- movie/movie.py
+++ movie/movie.py
@@ -4,16 +4,31 @@ import movie_pb2
 import movie_pb2_grpc
 import json
 
+
 class MovieServicer(movie_pb2_grpc.MovieServicer):
 
     def __init__(self):
         with open('{}/data/movies.json'.format("."), "r") as jsf:
             self.db = json.load(jsf)["movies"]
 
+    def GetMovieByID(self, request, context):
+        for movie in self.db:
+            if movie['id'] == request.id:
+                print("Movie found!")
+                return movie_pb2.MovieData(title=movie['title'], rating=movie['rating'], director=movie['director'],
+                                           id=movie['id'])
+        return movie_pb2.MovieData(title="", rating="", director="", id="")
+
+    def GetListMovies(self, request, context):
+        for movie in self.db:
+            yield movie_pb2.MovieData(title=movie['title'], rating=movie['rating'], director=movie['director'],
+                                      id=movie['id'])
+
+
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     movie_pb2_grpc.add_MovieServicer_to_server(MovieServicer(), server)
-    server.add_insecure_port('[::]:3001')
+    server.add_insecure_port('localhost:3001')
     server.start()
     server.wait_for_termination()
 
