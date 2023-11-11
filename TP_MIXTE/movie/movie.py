c732b9ef7e910a4ab2e4d7ae7eea52e3402567ce
Titouan Cocheril <titouan.cocheril25@gmail.com>
2023-11-11 14:25:22 +0100

    commit

--- TP_MIXTE/movie/movie.py
+++ TP_MIXTE/movie/movie.py
@@ -1,5 +1,4 @@
 from ariadne import graphql_sync, make_executable_schema, load_schema_from_path, ObjectType, QueryType, MutationType
-from ariadne.constants import PLAYGROUND_HTML
 from flask import Flask, request, jsonify, make_response
 
 import resolvers as r
@@ -36,10 +35,6 @@ def home():
 
 #####
 # graphql entry points
-
-@app.route('/graphql', methods=['GET'])
-def playground():
-    return PLAYGROUND_HTML, 200
 
 @app.route('/graphql', methods=['POST'])
 def graphql_server():
