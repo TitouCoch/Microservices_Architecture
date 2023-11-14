d867ad6c738bbfc279423f9fc4ec068de2b0ae2f
EliasMorio <elias14160@gmail.com>
2023-11-14 15:24:56 +0100

    refactor(movie):  add repository for objects data access

--- TP_MIXTE/movie/resolvers.py
+++ TP_MIXTE/movie/resolvers.py
@@ -1,90 +1,51 @@
 import json
 import uuid
+from utilities import MovieRepository, ActorRepository
+
+movie_repository = MovieRepository()
+actor_repository = ActorRepository()
 
 
 def movie_with_id(_, info, _id):
-    with open('{}/data/movies.json'.format("."), "r") as file:
-        movies = json.load(file)
-        for movie in movies['movies']:
-            if movie['id'] == _id:
-                return movie
+    return movie_repository.get_by_field('id', _id)
 
 
 def update_movie_rate(_, info, _id, _rating):
-    newmovies = {}
-    newmovie = {}
-    with open('{}/data/movies.json'.format("."), "r") as rfile:
-        movies = json.load(rfile)
-        for movie in movies['movies']:
-            if movie['id'] == _id:
-                movie['rating'] = _rating
-                newmovie = movie
-                newmovies = movies
-    with open('{}/data/movies.json'.format("."), "w") as wfile:
-        json.dump(newmovies, wfile)
-    print(newmovie)
-    return newmovie
+    movie_repository.update_rating(_id, _rating)
+    return movie_repository.get_by_field('id', _id)
 
 
 def resolve_actors_in_movie(movie, info):
-    with open('{}/data/actors.json'.format("."), "r") as file:
-        data = json.load(file)
-        actors = [actor for actor in data['actors'] if movie['id'] in actor['films']]
-        return actors
+    actors = []
+    for actor in actor_repository.get_all():
+        if movie['id'] in actor['films']:
+            actors.append(actor)
+    return actors
 
 
 def resolve_films_in_actor(actor, info):
-    with open('{}/data/movies.json'.format("."), "r") as file:
-        data = json.load(file)
-        movies = [movie for movie in data['movies'] if movie['id'] in actor['films']]
-        return movies
+    movies = []
+    for movie_id in actor['films']:
+        movies.append(movie_repository.get_by_field('id', movie_id))
+    return movies
 
 
 def actor_with_id(_, info, _id):
-    with open('{}/data/actors.json'.format("."), "r") as file:
-        data = json.load(file)
-        for actor in data['actors']:
-            if actor['id'] == _id:
-                return actor
+    return actor_repository.get_by_field('id', _id)
 
 
 def all_movies(_, info):
-    with open('{}/data/movies.json'.format("."), "r") as file:
-        data = json.load(file)
-        print(data)
-        return data['movies']
+    return movie_repository.get_all()
 
 
 def movie_with_title(_, info, _title):
-    with open('{}/data/movies.json'.format("."), "r") as file:
-        movies = json.load(file)['movies']
-        for movie in movies:
-            if str(movie['title']) == str(_title):
-                return movie
+    return movie_repository.get_by_field('title', _title)
 
 
 def del_movie_with_id(_, info, _id):
-    with open('{}/data/movies.json'.format("."), "r") as file:
-        movies = json.load(file)
-        for movie in movies['movies']:
-            if str(movie["id"]) == str(_id):
-                movies["movies"].remove(movie)
-    with open('{}/data/movies.json'.format("."), "w") as wfile:
-        json.dump(movies, wfile)
-        return movies["movies"]
+    return movie_repository.delete(_id)
 
 
 def add_movie(_, info, _movie):
-    with open('{}/data/movies.json'.format("."), "r") as file:
-        movies = json.load(file)
-        for movie in movies['movies']:
-            if str(movie["title"]) != str(_movie["title"]):
-                movie_uuid = str(uuid.uuid4())
-                _movie["id"] = movie_uuid
-                movies["movies"].append(_movie)
-    with open('{}/data/movies.json'.format("."), "w") as wfile:
-        json.dump(movies, wfile)
-    return movies['movies']
-
-
-
+    _movie['id'] = str(uuid.uuid4())
+    return movie_repository.add(_movie)
---
+++ TP_MIXTE/movie/utilities.py
@@ -0,0 +1,80 @@
+import json
+
+
+class MovieRepository:
+    movies: list = []
+
+    def __init__(self):
+        with open('{}/data/movies.json'.format("."), "r") as file:
+            data = json.load(file)
+            for movie in data['movies']:
+                self.movies.append(movie)
+
+    def get_all(self):
+        return self.movies
+
+    def get_by_field(self, field, value):
+        for movie in self.movies:
+            if movie[field] == value:
+                return movie
+        raise Exception("Movie not found")
+
+    def add(self, movie):
+        self.movies.append(movie)
+        self.save()
+        return movie
+
+    def delete(self, id):
+        self.movies = [movie for movie in self.movies if movie['id'] != id]
+        self.save()
+
+    def update_rating(self, id, rating):
+        for movie in self.movies:
+            if movie['id'] == id:
+                movie['rating'] = rating
+                print(movie, rating)
+        self.save()
+
+    def save(self):
+        with open('{}/data/movies.json'.format("."), "w") as wfile:
+            json.dump({"movies": self.movies}, wfile)
+
+
+class ActorRepository:
+
+    def __init__(self):
+        with open('{}/data/actors.json'.format("."), "r") as file:
+            data = json.load(file)
+            self.actors = data['actors']
+
+    def get_all(self):
+        return self.actors
+
+    def get_by_field(self, field, value):
+        for actor in self.actors:
+            if actor[field] == value:
+                return actor
+        return None
+
+    def add(self, actor):
+        self.actors.append(actor)
+        self.save()
+        return actor
+
+    def save(self):
+        with open('{}/data/actors.json'.format("."), "w") as wfile:
+            json.dump({"actors": self.actors}, wfile)
+
+    def exist(self, actor):
+        for a in self.actors:
+            if ((hasattr(actor, 'id') and a['id'] == actor.id)
+                    or (hasattr(actor, 'firstname') and a['firstname'] == actor.firstname
+                        and hasattr(actor, 'lastname') and a['lastname'] == actor.lastname)):
+                return True
+        return False
+
+    def add_movie(self, actor_id, movie_id):
+        for actor in self.actors:
+            if actor['id'] == actor_id:
+                actor['films'].append(movie_id)
+        self.save()
