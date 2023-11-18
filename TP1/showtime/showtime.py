6db344bcb5ec6547dcfb749ec66e76c7d9899b5f
Titouan Cocheril <titouan.cocheril25@gmail.com>
2023-11-18 20:03:26 +0100

    refacto TP1

--- TP1/showtime/showtime.py
+++ TP1/showtime/showtime.py
@@ -1,6 +1,5 @@
-from flask import Flask, render_template, request, jsonify, make_response
+from flask import Flask, jsonify, make_response
 import json
-from werkzeug.exceptions import NotFound
 
 app = Flask(__name__)
 
@@ -8,26 +7,29 @@ PORT = 3202
 HOST = '0.0.0.0'
 
 with open('{}/databases/times.json'.format("."), "r") as jsf:
-   schedules = json.load(jsf)["schedule"]
+    schedules = json.load(jsf)["schedule"]
+
 
 @app.route("/", methods=['GET'])
 def home():
-   return "<h1 style='color:blue'>Welcome to the Showtime service!</h1>"
+    return "<h1 style='color:blue'>Welcome to the Showtime service!</h1>"
+
 
 @app.route("/showtimes", methods=['GET'])
 def get_schedule():
     res = make_response(jsonify(schedules), 200)
     return res
 
+
 @app.route("/showmovies/<date>", methods=['GET'])
 def get_movies_bydate(date):
-   for schedule in schedules:
-      if str(schedule["date"]) == str(date):
-         res = make_response(jsonify(schedule), 200)
-         return res
-   return make_response(jsonify({"error": "Movie ID not found"}), 400)
+    for schedule in schedules:
+        if str(schedule["date"]) == str(date):
+            res = make_response(jsonify(schedule), 200)
+            return res
+    return make_response(jsonify({"error": "Movie ID not found"}), 400)
 
 
 if __name__ == "__main__":
-   print("Server running in port %s"%(PORT))
-   app.run(host=HOST, port=PORT)
+    print("Server running in port %s" % PORT)
+    app.run(host=HOST, port=PORT)
